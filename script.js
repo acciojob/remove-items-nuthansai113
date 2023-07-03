07b334d62b153f1c9c23df60cea11bd82bf1cb00
nuthansai113 <130201140+nuthansai113@users.noreply.github.com>
2023-07-03 22:00:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-//your JS code here. If required.
-
-//your JS code here. If required.
 function func_remove(){
    const elem = document.getElementById('colorSelect');
    const pick =  elem.remove(elem.selectedIndex);
